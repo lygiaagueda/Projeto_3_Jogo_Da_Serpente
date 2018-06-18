61ecfe5b2cc63efa022509443a603aff745b32fb
renatosantos0x00 <renato.santmont@gmail.com>
2018-06-18 10:12:25 -0300

    Placar

--- class/Placar.h
+++ class/Placar.h
@@ -1,1 +1,22 @@
+#ifndef PLACAR_H
+#define PLACAR_H
 
+#include <fstream>
+#include <string>
+
+class Placar{
+	private:
+		std::ofstream salvarPlacar;
+		std::ifStream recuperarPalcar;
+
+	public:
+		Placar();
+		virtual~Placar();
+	
+		void salvarPlacar(std::string placar);	
+		std::string recuperarPlacar(void);
+
+};
+
+
+#endif
--- src/Placar.cpp
+++ src/Placar.cpp
@@ -1,1 +1,8 @@
+#include "Placar.h"
 
+
+Placar::Placar();
+Placar::~Placar();
+
+void Placar::salvarPlacar(std::string placar);	
+std::string Placar::recuperarPlacar(void);
